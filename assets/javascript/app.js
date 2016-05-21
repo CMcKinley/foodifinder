40ad972dc971d7997ad00835afc99f97d2aedf4d
Caleb McKinley <calebcmckinley@gmail.com>
2016-05-21 19:31:29 -0400

    google maps render and mark

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,26 +1,91 @@
 $(document).ready(function(){
+var user;
+var lat;
+var long;
 
-function getRecipe(searchTerm){
-	//Dawns API code
-}	
+navigator.geolocation.getCurrentPosition(function(position) { 
+console.log(position)
+lat = position.coords.latitude
+long = position.coords.longitude;
+map = new google.maps.Map(document.getElementById('map'), {
+  center: {lat: lat, lng: long},
+  zoom: 10
+
+});
+
+})
+$('#mark').on('click', function(){
+if (user){
+	mark();
+}
+else{
+	login();
+}
+})
+
+
+
 
 function getNutrition(searchTerm){
 	//Johns API code
 }
 
-function renderTrending(){
-	//renders trending buttons
-}
 
-function renderSuggested(appRef, userRef, userid){
-	//renders suggested recipes based off users last searched
-	var lastSearch = userRef.searched
-	//need to call api functions and jquery recipes
-}
+
+
 
 
 //click function logs  user in with google
 $('body').on('click', '#login', function(){
+	login();
+
+});
+
+//click function logs user out
+$('body').on('click', '#logout', function(){
+
+	var appRef = new Firebase('https://foodifinder.firebaseio.com/');
+	firebase.auth().signOut().then(function() {
+	  $('#auth').html('<a class="waves-effect waves-light btn" id="login">Login</a>')
+	  userRef = appRef.child('users').child(userid);
+           userRef.update({status: 'loggedOut'});
+	}, function(error) {
+	  // An error happened.
+	});
+	
+});
+
+//click function runs search and stores user search
+$('.search').on('click', function(){
+	var appRef = new Firebase('https://foodifinder.firebaseio.com/');
+	var searchTerm = $('.input').val.trim();
+	userRef = appRef.child('users').child(userid);
+	//need to call getNutrition and getRecipe
+});
+
+//click function runs search on trending buttons
+
+
+//links the uid given by google to an id in our firebase app
+function linkAuthwReal(userid, appRef) {
+	appRef.once("value", function(snapshot) {
+		var userExists = snapshot.child('users').child(userid).exists();
+		var userRef;
+		if(userExists){  
+           userRef = appRef.child('users').child(userid);
+           userRef.update({status: 'loggedIn'});
+           console.log(userRef);
+        }
+
+        else{  userRef = appRef.child('users').child(userid);
+        	   userRef.set({userid: userid});
+              console.log(userRef);
+        }
+	});
+
+}
+
+function login(){
 	var appRef = new Firebase('https://foodifinder.firebaseio.com/');
 	var provider = new firebase.auth.GoogleAuthProvider();
 	firebase.auth().signInWithPopup(provider).then(function(result) {
@@ -28,7 +93,7 @@ $('body').on('click', '#login', function(){
 		var token = result.credential.accessToken;
 		console.log(token);
 		// The signed-in user info.
-		var user = result.user;
+		 user = result.user;
 		console.log(user);
 		console.log(user.uid);
 		firebase.auth().onAuthStateChanged(function(user) {
@@ -54,61 +119,26 @@ $('body').on('click', '#login', function(){
 	  var credential = error.credential;
 	  // ...
 	});
-
-});
-
-//click function logs user out
-$('body').on('click', '#logout', function(){
-
-	var appRef = new Firebase('https://foodifinder.firebaseio.com/');
-	firebase.auth().signOut().then(function() {
-	  $('#auth').html('<a class="waves-effect waves-light btn" id="login">Login</a>')
-	  userRef = appRef.child('users').child(userid);
-           userRef.update({status: 'loggedOut'});
-	}, function(error) {
-	  // An error happened.
-	});
-	
-});
-
-//click function runs search and stores user search
-$('.search').on('click', function(){
-	var appRef = new Firebase('https://foodifinder.firebaseio.com/');
-	var searchTerm = $('.input').val.trim();
-	userRef = appRef.child('users').child(userid);
-	userRef.update({searched: searchTerm});
-	//need to call getNutrition and getRecipe
-});
-
-//click function runs search on trending buttons
-$('.trending').on('click', function(){
-	var appRef = new Firebase('https://foodifinder.firebaseio.com/');
-	var trending = $(this).data('text');
-	userRef = appRef.child('users').child(userid);
-	userRef.update({searched: trending});
-	//need to call getNutrition and getRecipe
-});
-
-//links the uid given by google to an id in our firebase app
-function linkAuthwReal(userid, appRef) {
-	appRef.once("value", function(snapshot) {
-		var userExists = snapshot.child('users').child(userid).exists();
-		var userRef;
-		if(userExists){  
-           userRef = appRef.child('users').child(userid);
-           userRef.update({status: 'loggedIn'});
-           console.log(userRef);
-        }
-
-        else{  userRef = appRef.child('users').child(userid);
-        	   userRef.set({userid: userid});
-              console.log(userRef);
-        }
-	});
-
 }
 
+function mark(){
+	var marker = new google.maps.Marker({
+    position: {lat: lat, lng: long},
+    animation: google.maps.Animation.DROP,
+    map: map,
+    title: 'Hello World!'
 
+  });
+	function toggleBounce() {
+  if (marker.getAnimation() !== null) {
+    marker.setAnimation(null);
+  } else {
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+  }
+}
+marker.addListener('click', toggleBounce);
+
+}
 
 
 });
